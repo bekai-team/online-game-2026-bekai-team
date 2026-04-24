41344f594cf3cd7468cebc1a59510448802d2234
karakarakr <titan1234eti@gmail.com>
2026-04-24 10:13:22 +0300

    feature(feature/auth): add entity for user with enum-roles

--- api/src/user/entities/user.entity.ts
+++ api/src/user/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from '../enums/user-role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 50 })
+  username: string;
+
+  @Column({ length: 200 })
+  email: string;
+
+  @Column({ length: 100 })
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+}
