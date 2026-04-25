29698ec6fd67dc246d2e74fb6579181921eb3a67
karakarakr <titan1234eti@gmail.com>
2026-04-25 15:23:02 +0300

    feature(feature/auth): add sign-up logic

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { UserData } from 'src/shared/interface/user-data.interface';
 
 @Injectable()
 export class AuthService {
@@ -14,15 +15,36 @@ export class AuthService {
   ) {}
 
   async signUp(signUpDto: SignUpDto) {
-    return await ':D';
+    const existedUserByEmail = await this.userService.findByEmail(signUpDto.email);
+
+    console.log(existedUserByEmail);
+
+    if (existedUserByEmail['email']) {
+      throw new HttpException('User with such email exists!', 400);
+    }
+
+    if (existedUserByEmail['username']) {
+      throw new HttpException('User with such username exists!', 400);
+    }
+
+    const user = await this.userService.create(signUpDto);
+
+    return {
+      message: 'User has been created successfuly!',
+    };
   }
 
   async login(loginDto: LoginDto) {
     const user = await this.userService.findByEmail(loginDto.email);
+
+    if (!user['email']) {
+      throw new UnauthorizedException('Email does not exist!');
+    }
+
     const isMatchedPassword = await bcrypt.compare(loginDto.password, user['password']);
 
     if (!isMatchedPassword) {
-      throw new UnauthorizedException('Wrong password');
+      throw new UnauthorizedException('Wrong password!');
     }
 
     const payload = { sub: user['id'], username: user['username'], email: user['email'] };
