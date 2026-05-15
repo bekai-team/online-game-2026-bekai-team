606d41781f6817faf22e24924ea4b398907ddee3
gting69 <emily.kirillova@gmail.com>
2026-05-16 01:01:41 +0300

    feat(feature/ui-updates): налаштовано UI-якорі та адаптовано LocalStorage для web

--- client/scripts/login_req.gd
+++ client/scripts/login_req.gd
@@ -6,6 +6,14 @@ func _on_pressed() -> void:
 
 func _on_request_completed(result, response_code, headers, body):
 	if response_code == 200:
-		print("Login successful: ", body.get_string_from_utf8())
+		var response = JSON.parse_string(body.get_string_from_utf8())
+		
+		if response and response.has("accessToken"):
+			var token = response.get("accessToken")
+			
+			SessionManager.save_token(token)
+			
+			get_tree().change_scene_to_file("res://scenes/world.tscn")
+			
 	else:
 		print("Login failed with response code: ", response_code)
--- client/scripts/session_manager.gd
+++ client/scripts/session_manager.gd
@@ -1,36 +1,30 @@
 extends Node
 
-const SAVE_PATH = "user://session.json"
 var access_token = ""
-
-func _ready():
-	load_token()
 
 func save_token(token: String):
 	access_token = token
-	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	if file:
-		var data = {"token": token}
-		file.store_string(JSON.stringify(data))
-		file.close()
-		print("Токен успішно збережено в LocalStorage!")
+	if OS.has_feature("web"):
+		JavaScriptBridge.eval("localStorage.setItem('accessToken', '%s');" % token)
+		print("Токен збережено в браузерний LocalStorage")
+	else:
+		var file = FileAccess.open("user://session.json", FileAccess.WRITE)
+		if file:
+			file.store_string(JSON.stringify({"token": token}))
+			file.close()
 
-func load_token():
-	if FileAccess.file_exists(SAVE_PATH):
-		var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-		var content = file.get_as_text()
-		file.close()
-		
-		var data = JSON.parse_string(content)
-		if data and data.has("token"):
-			access_token = data["token"]
-			print("Сесію відновлено! Знайдено токен.")
+func load_token() -> bool:
+	if OS.has_feature("web"):
+		var token = JavaScriptBridge.eval("localStorage.getItem('accessToken');")
+		if token != null and token != "":
+			access_token = token
+			print("Токен відновлено з LocalStorage")
 			return true
+	else:
+		if FileAccess.file_exists("user://session.json"):
+			var file = FileAccess.open("user://session.json", FileAccess.READ)
+			var data = JSON.parse_string(file.get_as_text())
+			if data and data.has("token"):
+				access_token = data["token"]
+				return true
 	return false
-
-func clear_token():
-	access_token = ""
-	var dir = DirAccess.open("user://")
-	if dir and dir.file_exists("session.json"):
-		dir.remove("session.json")
-		print("Токен видалено, сесію закрито.")
