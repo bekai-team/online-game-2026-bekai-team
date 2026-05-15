f568ec243beea2bc460626df44c0668b369f8712
karakarakr <titan1234eti@gmail.com>
2026-05-15 08:38:30 +0300

    chore(feature/basic-endpoints): generate boilerplate CRUD-endpoints for NPC

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import { HttpLoggerMiddleware } from './middlewares/http-logger/http-logger.middleware';
+import { NpcModule } from './npc/npc.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { HttpLoggerMiddleware } from './middlewares/http-logger/http-logger.midd
     DatabaseModule,
     TerminusModule,
     HttpModule,
+    NpcModule,
   ],
   controllers: [AppController],
   providers: [AppService],
