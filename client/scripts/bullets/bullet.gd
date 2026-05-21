076791537afb1a7a8c1e28c24fbb78a76b73c528
MrMOON <titan1234eti@gmail.com>
2026-05-21 12:41:17 +0300

    fix(bugfix/bullet): decreased melee hitbox temporary

--- client/scripts/bullets/bullet.gd
+++ client/scripts/bullets/bullet.gd
@@ -12,9 +12,12 @@ func _process(delta: float) -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	var is_body_box = body.name == 'BodyBox'
 	print("Bullet penetrted body: {}", body.get_canvas_item())
+	
+	if is_body_box:
+		print('HIT')
 	
 	if body.has_method("take_damage"):
 		body.take_damage(BULLET_DAMAGE)
