8e37a19cd8db9babb71efce2db4f001ac2e75a3c
gting69 <emily.kirillova@gmail.com>
2026-05-31 23:30:20 +0300

    Додано екран діалогу з npc

---
+++ client/dialogue.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var input_field = $ModalWindow/MainVBox/DialogueInput
+@onready var output_label = $ModalWindow/MainVBox/TopHBox/DialogueScroll/OutputLabel
+
+func _ready():
+	output_label.append_text("[b][color=#53EBE4]Stranger:[/color][/b] You know this is a very dangerous place.\n\n")
+
+func _input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER and input_field.has_focus():
+		_send_message()
+		get_viewport().set_input_as_handled()
+
+func _send_message():
+	var msg = input_field.text.strip_edges()
+	if msg == "": return
+	
+	output_label.append_text("[b]Player:[/b] " + msg + "\n")
+	input_field.clear()
