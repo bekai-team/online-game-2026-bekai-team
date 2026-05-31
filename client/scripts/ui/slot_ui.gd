7a961e491eee4212b0f41637b6bec8bced0d4b97
MrMOON <titan1234eti@gmail.com>
2026-05-31 13:42:05 +0300

    feat(feature/inventory): add items stack label that counts amount of collected items

--- client/scripts/ui/slot_ui.gd
+++ client/scripts/ui/slot_ui.gd
@@ -2,15 +2,19 @@ extends Panel
 
 @onready var background_sprite: Sprite2D = $background
 @onready var item_sprite: Sprite2D = $CenterContainer/Panel/item
+@onready var amount_label: Label = $CenterContainer/Panel/Label
 
-func update(item: InventoryItem) -> void:
-	if !item:
+func update(slot: InventorySlot) -> void:
+	if !slot.item:
 		background_sprite.frame = 0
 		item_sprite.visible = false
+		amount_label.visible = false
 	else:
 		background_sprite.frame = 1
 		item_sprite.visible = true
-		item_sprite.texture = item.texture
+		item_sprite.texture = slot.item.texture
+		amount_label.visible = true
+		amount_label.text = str(slot.amount)
 		
 		var sprite_scale_x = item_sprite.scale.x
 		var width: float = item_sprite.get_rect().size.x
