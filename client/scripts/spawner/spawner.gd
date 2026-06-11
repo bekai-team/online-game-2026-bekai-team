433c5d6a0a7d88046c8e00838c7507b169179c42
MrMOON <titan1234eti@gmail.com>
2026-06-11 11:25:30 +0300

    feat(feature/spawner): add active spawner bool flag & is destroyable flag

--- client/scripts/spawner/spawner.gd
+++ client/scripts/spawner/spawner.gd
@@ -3,14 +3,17 @@ extends Node2D
 @export var spawn_object: PackedScene
 @export var followed_to: Node2D
 @export_range(1, 100) var count: int = 1
+@export var is_active: bool = true
+@export var is_destroyable_on_enter: bool = false
 var rng = RandomNumberGenerator.new()
 @onready var radius = $SpawnArea/CollisionShape2D.shape.radius
 
 func _on_spawn_area_area_entered(area: Area2D) -> void:
-	if area.name != 'PickUpBox':
+	if area.name != 'PickUpBox' or !is_active:
 		return
 		
 	if spawn_object:
+		is_active = false
 		for i in range(count):
 			var new_spawn = spawn_object.instantiate()
 			var random_angle = randf_range(0, TAU)
@@ -24,3 +27,6 @@ func _on_spawn_area_area_entered(area: Area2D) -> void:
 			new_spawn.Goal = followed_to
 			
 			get_tree().current_scene.add_child(new_spawn)
+	
+	if is_destroyable_on_enter:
+		queue_free()
