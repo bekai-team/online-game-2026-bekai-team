a24a8b0dfe870f8b34a40b70a2530ee8bc048ad0
MrMOON <titan1234eti@gmail.com>
2026-06-11 11:20:40 +0300

    feat(feature/spawner): add random spawn position within a circle & spawn count

--- client/scripts/spawner/spawner.gd
+++ client/scripts/spawner/spawner.gd
@@ -2,15 +2,25 @@ extends Node2D
 
 @export var spawn_object: PackedScene
 @export var followed_to: Node2D
+@export_range(1, 100) var count: int = 1
+var rng = RandomNumberGenerator.new()
+@onready var radius = $SpawnArea/CollisionShape2D.shape.radius
 
 func _on_spawn_area_area_entered(area: Area2D) -> void:
-	if area.name == 'PickUpBox':
-		print("entered player!")
+	if area.name != 'PickUpBox':
+		return
 		
-		if spawn_object:
+	if spawn_object:
+		for i in range(count):
 			var new_spawn = spawn_object.instantiate()
-			new_spawn.global_position = global_position
+			var random_angle = randf_range(0, TAU)
+			var random_distance = sqrt(randf()) * radius
 			
+			var spawn_offset = Vector2(
+				cos(random_angle), sin(random_angle)
+			) * random_distance
+			
+			new_spawn.global_position = global_position + spawn_offset
 			new_spawn.Goal = followed_to
 			
 			get_tree().current_scene.add_child(new_spawn)
