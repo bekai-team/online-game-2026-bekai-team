954003dcf475989766e16c1e647ede5435fe4858
MrMOON <titan1234eti@gmail.com>
2026-06-12 13:15:09 +0300

    feat(feature/enemies): add damage attack

--- client/scripts/enemies/rat.gd
+++ client/scripts/enemies/rat.gd
@@ -9,7 +9,11 @@ var current_hp = 100
 @onready var health_bar = $ProgressBar
 @onready var nav_agent = $NavigationAgent2D
 @onready var sprite = $AnimatedSprite2D
-@export var Goal: Node = null
+@export var Goal: Node2D = null
+@export var damage: int = 2
+@export var area_target_name: String = 'PickUpBox'
+var entered_area: bool = false
+var is_dangerous = true
 
 func _ready() -> void:
 	if is_instance_valid(Goal):
@@ -38,6 +42,8 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = new_velocity
 	move_and_slide()
+	attack()
+	
 
 func play_animations(move_dir: Vector2) -> void:
 	if move_dir.length() < 0.1:
@@ -57,8 +63,24 @@ func take_damage(amount):
 	current_hp -= amount
 	health_bar.value = current_hp
 	
-	print("Щур отримав ", amount, " шкоди! Залишилось HP: ", current_hp)
-	
 	if current_hp <= 0:
 		print("Кілограм щура")
 		queue_free()
+		
+func attack():
+	if entered_area and Goal.has_method('take_damage'):
+		print('Is dangerous: ', entered_area)
+		if !is_dangerous:
+			return
+		Goal.take_damage(damage)
+		is_dangerous = false
+		await get_tree().create_timer(2.0).timeout
+		is_dangerous = true
+
+func _on_attack_box_area_entered(area: Area2D) -> void:
+	if area.name == area_target_name:
+		entered_area = true
+
+func _on_attack_box_area_exited(area: Area2D) -> void:
+	if area.name == area_target_name:
+		entered_area = false
