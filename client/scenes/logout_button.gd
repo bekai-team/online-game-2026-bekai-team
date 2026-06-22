adf727b9df7fa9f7866e1298b464a5b59e2fe8d4
karakarakr <titan1234eti@gmail.com>
2026-06-22 15:47:23 +0300

    bugfix(feature/dialogue-ui): set 201 status code to be detected as success

--- client/scenes/logout_button.gd
+++ client/scenes/logout_button.gd
@@ -1,21 +1,17 @@
 extends Button
 
 @onready var http_request: HTTPRequest = $HTTPRequest
-var logout_url = "http://localhost:3001/api/auth/login"
+var logout_url = "http://localhost:3001/api/auth/logout"
 
 func _ready():
 	http_request.request_completed.connect(_on_http_request_request_completed)
 
 func _on_pressed() -> void:
-	#var json_data = JSON.stringify(
-		#{'accessToken': SessionManager.access_token}
-	#)
-	#var headers = ["Content-Type: application/json"]
-	#http_request.request(
-		#logout_url, 
-		#headers, 
-		#HTTPClient.METHOD_POST, 
-		#json_data)
+	var headers = ["Authorization: Bearer %s" % SessionManager.access_token]
+	http_request.request(
+		logout_url, 
+		headers, 
+		HTTPClient.METHOD_POST)
 	SessionManager.clear_data()
 	visible = false
 	
@@ -24,12 +20,8 @@ func _on_http_request_request_completed(
 	response_code: int, 
 	headers: PackedStringArray, 
 	body: PackedByteArray) -> void:
-	if response_code == 200:
+	if response_code == 200 or response_code == 201:
 		var response_data = JSON.parse_string(body.get_string_from_utf8())
-		
-		if response_data:
-			OS.alert('Logout successful!')
-			
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	else:
 		OS.alert("Помилка (Код: " + str(response_code) + ")")
