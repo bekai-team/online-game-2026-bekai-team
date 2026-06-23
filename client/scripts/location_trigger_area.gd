7be9f47dd0c5a81c0d18877b6f570d5c0c56f531
karakarakr <titan1234eti@gmail.com>
2026-06-23 10:01:44 +0300

    fix(feature/level-design): change to export string to avoid recursive loading of scenes

--- client/scripts/location_trigger_area.gd
+++ client/scripts/location_trigger_area.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
 @export var target_area_name: String = 'PickUpBox'
-@export var target_level_to_load: PackedScene
+@export_file("*.tscn") var target_level_path: String
 
 func _on_area_entered(area: Area2D) -> void:
-	#var belongs_to_player = 
-	print(area.get_parent())
 	if area.name == target_area_name:
-		get_tree().change_scene_to_packed(target_level_to_load)
+		if target_level_path != "":
+			get_tree().change_scene_to_file(target_level_path)
+		else:
+			push_error("ERROR (target_level_path) wasn't set in inspector!")
